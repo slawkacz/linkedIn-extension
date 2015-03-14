629173187838841673655df8ac8af7acc85c1ce9
slawkacz <slawkacz@gmail.com>
2015-03-14 17:35:52 +0100

    * Added libs to gitignore * Added showing modal * Added communication with background process * Added propagating Person Object with defaults

--- app/scripts/content/contentscript.js
+++ app/scripts/content/contentscript.js
@@ -1,41 +1,76 @@
 'use strict';
-(function(){
-
-  var name = document.querySelector('.full-name');
-  var plusModal = null;
-  var showPlusModal = function(){
-
-  };
-  var addPlusButton = function(){
-    var button = document.createElement('button');
-    button.textContent = "+";
-    button.addEventListener('click',showPlusModal);
-    name.parentNode.appendChild(button);
-  };
-  var addPlusModal = function(){
-    var tempDiv = document.createElement('div');
-    tempDiv.innerHTML = plusModalTemplate;
-    console.log(tempDiv.childNodes[0]);
-    document.body.appendChild(tempDiv.childNodes[0]);
-  };
-
-  var init = function(){
-    getTemplates();
-    addPlusButton();
-    addPlusModal();
-  };
-  var getTemplates = function(){
-    var xmlHttp = null;
-    xmlHttp = new XMLHttpRequest();
-    console.log(chrome);
-    xmlHttp.open( "GET", chrome.extension.getURL ("scripts/content/templates.html"), false );
-    xmlHttp.send( null );
-    var inject  = document.createElement("div");
-    inject.innerHTML = xmlHttp.responseText;
-    document.body.insertBefore (inject, document.body.firstChild);
-
-  };
-  init();
-
+(function() {
+    var name = document.querySelector('.full-name');
+    var plusModal = false;
+    var showPlusModal = function() {
+        if (!plusModal) {
+            plusModal = document.querySelector('#linkedInExt-personForm');
+        }
+        plusModal.classList.add('show');
+    };
+    var addPlusButton = function() {
+        var button = document.createElement('button');
+        button.textContent = '+';
+        button.addEventListener('click', showPlusModal);
+        name.parentNode.appendChild(button);
+    };
+    var editPerson = function(e) {
+        e.preventDefault();
+    };
+    var attachTemplateEvents = function() {
+        plusModal = document.querySelector('#linkedInExt-personForm');
+        plusModal.addEventListener('submit', editPerson);
+        plusModal.querySelector('.linkedInExt-close').addEventListener('click', function(e) {
+            e.preventDefault();
+            plusModal.classList.remove('show');
+        });
+    };
+    var Profile = {
+        name: null,
+        email: null,
+        city: null,
+        status: null,
+        comment: null,
+        tags: [],
+    };
+    var setProfile = function(data) {
+        console.log(NAME_EL);
+        if (!data) {
+            Profile.name = document.querySelector(NAME_EL).textContent;
+            Profile.city = document.querySelector(LOCAL_EL).textContent;
+        } else {
+            Profile = data;
+        };
+        console.log(Profile);
+    };
+    var init = function() {
+        var profileId = window.location.search.match(/id=(\d+)/)[1]
+        chrome.runtime.sendMessage({
+            msg: 'getProfile',
+            id: profileId
+        }, function(res) {
+            setProfile(res);
+            getTemplates().then(attachTemplateEvents);
+            attachTemplateEvents();
+            addPlusButton();
+        });
+    };
+    var getTemplates = function() {
+        return new Promise(function(resolve, reject) {
+            try {
+                var xmlHttp = null,
+                    scriptSrc = chrome.extension.getURL('scripts/content/templates.html');
+                xmlHttp = new XMLHttpRequest();
+                xmlHttp.open('GET', scriptSrc, false);
+                xmlHttp.send(null);
+                var inject = document.createElement('div');
+                inject.innerHTML = xmlHttp.responseText;
+                document.body.insertBefore(inject, document.body.firstChild);
+                resolve(true);
+            } catch (error) {
+                reject(error);
+            }
+        });
+    };
+    init();
 })();
-

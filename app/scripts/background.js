0e226c436cf7e3b1c758ddef78850597da58e2ce
slawkacz <slawkacz@gmail.com>
2015-03-14 17:31:30 +0100

    added message event and getProfile/setProfile functions interfaces

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,6 +1,27 @@
 'use strict';
-
-chrome.runtime.onInstalled.addListener(function (details) {
-  console.log('previousVersion', details.previousVersion);
+var getProfile = function(id) {
+    return false;
+};
+var setProfile = function(data) {
+    return true;
+}
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    console.log(request);
+    
+    var response = {};
+    switch (request.msg) {
+        case 'getProfile':
+            response = getProfile(request.id);
+            break;
+        case 'setProfile':
+            response = setProfile(request.profile);
+            break;
+        default:
+            console.log(request);
+            break;
+    }
+    sendResponse(response);
 });
-
+chrome.runtime.onInstalled.addListener(function(details) {
+    console.log('previousVersion', details.previousVersion);
+});
